59d2459d5de92325239a5e790e142b55b607bb56
jer bushau <exjerlock@hotmail.com>
2018-01-31 20:26:40 -0500

    Implemented hamburger nav functionality

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,14 +5,16 @@
         <img src="https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.flaticon.com%2Ficons%2Fpng%2F512%2F249%2F249209.png&f=1" width="75" height="75">
         Fit Together
       </a>
-      <button class="button navbar-burger">
+      <button class="button navbar-burger"
+              @click="toggleHamburgerMenu()">
         <span></span>
         <span></span>
         <span></span>
       </button>
     </div>
 
-    <div class="navbar-menu">
+    <div class="navbar-menu"
+         :class="{ 'is-active': isHamburgerActive }">
       <div class="navbar-end">
         <a class="navbar-item">Sign Out</a>
       </div>
@@ -22,9 +24,35 @@
 
 <script>
 export default {
-  name: 'MainNav'
+  name: 'MainNav',
+  data: function() {
+    return {
+      isHamburgerActive: false
+    }
+  },
+  methods: {
+    toggleHamburgerMenu: function() {
+      return this.isHamburgerActive = !this.isHamburgerActive
+    },
+  }
 }
 </script>
 
 <style scoped>
+.navbar-menu {
+  background: #555;
+  color: whitesmoke;
+  text-align: center;
+}
+.navbar-menu.is-active {
+  background: #333;
+}
+.navbar-item:hover {
+  background: whitesmoke;
+}
+.navbar-burger {
+  background: #444;
+  color:#fff;
+  border: 1px solid silver;
+}
 </style>
